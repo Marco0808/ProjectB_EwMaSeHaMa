d17f838de6c9888b334060446eaf162717960730
Marco Perschke <perschke.marco@gmail.com>
2021-05-05 11:56:15 +0200

    ADD Transparent and opaque default lit sprite materials; UP Render pipeline asset

--- Assets/Settings/InputActions.cs
+++ Assets/Settings/InputActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/InputActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Settings/InputActions.inputactions'
 
 using System;
 using System.Collections;
