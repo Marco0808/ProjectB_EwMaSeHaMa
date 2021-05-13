27ee8741d289472b278c2bde3f3e25ad0831c209
MatiHaufn <65949947+MatiHaufn@users.noreply.github.com>
2021-05-13 21:19:57 +0200

    Animationen der Player, NavMesh für Treppe, diverse Assets eingefügt, Portraits der Player

--- Assets/Scripts/Game/PlayerOrientation.cs
+++ Assets/Scripts/Game/PlayerOrientation.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,22 +9,62 @@ using UnityEngine.AI;
 public class PlayerOrientation : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer sprite;
 
     private NavMeshAgent _agent;
+    private Vector3 myScale; 
 
     int walkingState = 0; 
 
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+        myScale = transform.localScale;
     }
 
     void Update()
     {
         Vector3 moveDirection = _agent.steeringTarget - _agent.transform.position;
 
+        if(moveDirection.x < 0)
+        {
+            if (moveDirection.z < moveDirection.x)
+            {
+                walkingState = 2;
+            }
+            else
+            {
+                sprite.flipX = true;
+                //myScale.x = -Math.Abs(myScale.x);
+                walkingState = 1;
+            }
+        }
+        else if(moveDirection.x > 0)
+        {
+            if (moveDirection.z > moveDirection.x)
+            {
+                walkingState = 3;
+            }
+            else
+            {
+                sprite.flipX = false;
+                //myScale.x = Math.Abs(myScale.x); 
+                walkingState = 1; 
+            }
+        }
+        else
+        {
+            walkingState = 0; 
+        }
+        
+        
+        
+
+
+
         //Debug.Log("++++++++++ ANIMATOR: " + animator.runtimeAnimatorController);
-        //Debug.Log(moveDirection);
+        Debug.Log(moveDirection);
+        transform.localScale = myScale;
         animator.SetInteger("walkingState", walkingState); 
 
     }
